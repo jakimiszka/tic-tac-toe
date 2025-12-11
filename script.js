e001e4983dc5a776ce0148efa24b45e2b364866b
jakimiszka <jarek.jakimicha@gmail.com>
2025-12-11 22:20:46 +0100

    panels marks

--- script.js
+++ script.js
@@ -6,23 +6,34 @@ const newGameCPU = document.querySelector('#newGameCPU');
 const menu = document.querySelector('.menu');
 const board = document.querySelector('.board');
 const playerTurnImage = document.querySelector('.board--navigation__turn--image img');
-
+const oMark = './assets/icon-o.svg';
+const xMark = './assets/icon-x.svg';
 const game = new Game();
+
 const pannels = document.querySelectorAll('.board--panels .panel');
 pannels.forEach((panel, index) => {
     panel.addEventListener('click', () => {
-        console.log(index);
+
+        // Prevent further moves if game is over or panel is already marked
         if(game.isGameOver || game.board[Math.floor(index / 3)][index % 3] !== ''){
             return;
         }
-        panel.textContent = game.currentPlayer;
+        // fill the panel
+        const currentPlayer = game.currentPlayer;
+        if(currentPlayer === 'O')
+            panel.innerHTML = `<img src="${oMark}" alt="O Mark">`;
+        if(currentPlayer === 'X')
+            panel.innerHTML = `<img src="${xMark}" alt="O Mark">`;
+
+        // Update the game board
         game.board[Math.floor(index / 3)][index % 3] = game.currentPlayer;
         const result = game.checkWin();
+        
         if(result){
             if(result === 'Draw'){
-                alert("It's a draw!");
+                console.log("It's a draw!");
             } else {
-                alert(`Player ${result} wins!`);
+                console.log(`Player ${result} wins!`);
             }
         } else {
             const player = game.togglePlayer();
